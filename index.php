cdbbfa7fc17b950640ac34efc140eae547df286b
silviu <silviu.badiu@mail.com>
2013-02-02 20:19:33 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  *
  * Written by: djcrackhome (Sebastian Graebner) <sgraebner@my.canyons.edu>
  * License: I dedicate any and all copyright interest in this software to the public domain. I make this dedication for
- * the benefit of the public at large and to the detriment of my heirs and successors. I intend this dedication to be
+ * the benefcit of the public at large and to the detriment of my heirs and successors. I intend this dedication to be
  * an overt act of relinquishment in perpetuity of all present and future rights to this software under copyright law.
  */
 
